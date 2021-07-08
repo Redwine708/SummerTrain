e7937c12c651a92db2ca6a7a88772bdbf9d0c73a
Redwine708 <1802407593@qq.com>
2021-07-08 23:41:43 +0800

    add some problems

--- ExcerciseOne/10memoryManagement.cpp
+++ ExcerciseOne/10memoryManagement.cpp
@@ -1,7 +1,16 @@
 #include<iostream>
-using namespace std;
+#include<string>
+#include<vector>
+#include<unordered_map>
 
+const int ORIGIN_POS = 1;
+
+using namespace std;
+//Three operations alloc erase defragment
 int main()
 {
+    int operators = 0 ; 
+    int memorySize = 0;
+    cin>>operators>>memorySize;
     
 }
